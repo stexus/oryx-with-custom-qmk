e89f2f9eac57cf0ee181698dd7581f3e03f99e41
Massimo Tseng <massimo.tseng@gmail.com>
2025-05-25 02:11:50 -0700

    fix syntax

--- AEn50/keymap.c
+++ AEn50/keymap.c
@@ -228,5 +228,4 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
   'L', 'L', 'L', 'L', 'L', 'L',             'R', 'R', 'R', 'R', 'R', 'R',
   'L', 'L', 'L', 'L', 'L', 'L',             'R', 'R', 'R', 'R', 'R', 'R',
                            '*', '*',   '*', '*'
-  )
-);
+  );
