d6abf9a0b2edfac4fa5243cc716c0df703727d76
Massimo Tseng <massimo.tseng@gmail.com>
2025-05-26 11:36:09 -0700

    remove tap flow from space

--- AEn50/keymap.c
+++ AEn50/keymap.c
@@ -229,3 +229,17 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
   'L', 'L', 'L', 'L', 'L', 'L',             'R', 'R', 'R', 'R', 'R', 'R',
                            '*', '*',   '*', '*'
   );
+bool is_flow_tap_key(uint16_t keycode) {
+    if ((get_mods() & (MOD_MASK_CG | MOD_BIT_LALT)) != 0) {
+        return false; // Disable Flow Tap on hotkeys.
+    }
+    switch (get_tap_keycode(keycode)) {
+        case KC_A ... KC_Z:
+        case KC_DOT:
+        case KC_COMM:
+        case KC_SCLN:
+        case KC_SLSH:
+            return true;
+    }
+    return false;
+}
