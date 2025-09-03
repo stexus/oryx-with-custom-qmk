45ebfc43a1cca1ec739c2ac75e9e6b815cefd540
Massimo Tseng <massimo@rivosinc.com>
2025-09-03 11:00:14 -0700

    ensure enter lt works

--- rd6q7/keymap.c
+++ rd6q7/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,    KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,    KC_BSPC,        
     KC_ESCAPE,      KC_A,KC_S,KC_D,KC_F,KC_G,                                    KC_H,           KC_J,KC_K,KC_L,KC_SCLN,KC_QUOTE,       
     KC_LEFT_SHIFT,  KC_Z,KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RIGHT_SHIFT, 
-                                                    LT(1, KC_ENTER),CW_TOGG,                                MO(2),          KC_SPACE
+                                                    KC_ENTER,CW_TOGG,                                MO(2),          KC_SPACE
   ),
   [1] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           DF(0), 
@@ -222,6 +222,8 @@ smtd_resolution on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap
         SMTD_MT(KC_L, KC_RIGHT_ALT, 2)
         SMTD_MT(KC_SCLN, KC_RIGHT_CTRL, 2)
         SMTD_LT(KC_M, 8, 2)
+
+        SMTD_LT(KC_ENT, 1, 2)
     }
 
     return SMTD_RESOLUTION_UNHANDLED;
