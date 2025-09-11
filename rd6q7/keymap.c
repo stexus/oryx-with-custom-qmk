5bbce4bcc1c0cd7f1e4a45b475a00a75426fadc4
Massimo Tseng <massimo@rivosinc.com>
2025-09-11 16:06:56 -0700

    add back night

--- rd6q7/keymap.c
+++ rd6q7/keymap.c
@@ -37,7 +37,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    KC_CAPS,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT, 
+    KC_CAPS,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           DF(1), 
     KC_TAB,         KC_Q,           MT(MOD_LALT, KC_W),KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           MT(MOD_RALT, KC_O),KC_P,           KC_BSPC,        
     KC_ESCAPE,      MT(MOD_LCTL, KC_A),LT(7, KC_S),    MT(MOD_LGUI, KC_D),MT(MOD_LSFT, KC_F),LT(4, KC_G),                                    KC_H,           MT(MOD_RSFT, KC_J),MT(MOD_LGUI, KC_K),LT(8, KC_L),    MT(MOD_RCTL, KC_SCLN),KC_QUOTE,       
     CW_TOGG,        MT(MOD_LCTL, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_CAPS,        
