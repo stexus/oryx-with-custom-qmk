a246eefca344ab9366d68c814ccc61c8be6acff7
Massimo Tseng <massimo@rivosinc.com>
2025-09-13 15:48:54 -0700

    remove new shifts from flow tap

--- rd6q7/keymap.c
+++ rd6q7/keymap.c
@@ -211,6 +211,8 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_keycode) {
     if (is_flow_tap_key(keycode) && is_flow_tap_key(prev_keycode)) {
         switch (keycode) {
+            case MT(MOD_LSFT, KC_ESCAPE):
+            case MT(MOD_RSFT, KC_SLASH):
             case MT(MOD_LSFT, KC_F):
             case MT(MOD_RSFT, KC_J):
             case LT(7, KC_S):
